d5c738f84ba58e9f5f23184cebf0425eef7cd87c
Moringa School <chimolilucy@gmail.com>
2021-07-02 21:00:59 +0300

    adding category model to pictures/models.py

--- pictures/models.py
+++ pictures/models.py
@@ -14,3 +14,17 @@ class Location(models.Model):
     #     location = Location.objects.filter(id=id)
     #     location.update(country=name)
     #     return location
+
+class Category(models.Model):
+    category=models.CharField(max_length=50)
+    def __str__(self):
+        return self.category
+    # def save_category(self):
+    #     self.save()
+    # def delete_category(self):
+    #     self.delete()
+    # @classmethod
+    # def update(cls,id,name):
+    #     cate=Category.objects.filter(id=id)
+    #     cate.update(category=name)
+    #     return cate
