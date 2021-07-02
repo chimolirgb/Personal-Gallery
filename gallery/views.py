8ce9a965e67bd8b8cd2a7c2d96a863942d3af97f
Moringa School <chimolilucy@gmail.com>
2021-07-02 16:53:44 +0300

    creating a new view function images_of_day for returning images of that day in gallery/views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,14 @@
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
     return HttpResponse('Beauty is an Expression')
