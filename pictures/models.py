4068cc84a47ca8d312db6178413e3ddf6292d434
Moringa School <chimolilucy@gmail.com>
2021-07-02 20:48:25 +0300

    modified:db.sqlite3,gallery/urls.py,pictures/models.py,pictures/migration/0001_initial.py

--- pictures/models.py
+++ pictures/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     country=models.CharField(max_length=50)
-    # def __str__(self):
-    #     return self.country
+    def __str__(self):
+         return self.country
     # def save_location(self):
     #     self.save()
     # def delete_location(self):
