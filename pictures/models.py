1598175ebbef7cd56146cfd7955655364c22cc3b
Moringa School <chimolilucy@gmail.com>
2021-07-03 14:28:54 +0300

    modified:pictures/models.py

--- pictures/models.py
+++ pictures/models.py
@@ -35,6 +35,33 @@ class Image(models.Model):
     description = models.TextField()
     location = models.ForeignKey(Location,on_delete= models.DO_NOTHING)
     category = models.ForeignKey(Category,on_delete= models.DO_NOTHING)
+    def save_image(self):
+        self.save()
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.all()
+        return images
+    @classmethod
+    def search_by_category(cls,search_term):
+        image = cls.objects.filter(category__category__contains=search_term)
+        return image
+    @classmethod
+    def update(cls,id,name):
+        image=Image.objects.filter(id=id)
+        image.update(name=name)
+        return image
+
+    def delete_image(self):
+        self.delete()
+    @classmethod 
+    def get_image_by_id(cls,id):
+        image = Image.objects.get(id=id)
+        return image
+
+    @classmethod
+    def filter_location(cls,fil):
+        loc_image=Image.objects.filter(location__country__icointain=fil)
+        return loc_image
 
     
 
