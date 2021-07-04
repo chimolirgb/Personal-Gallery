fd891bb603d23596e38a741749349120c3d5d986
Moringa School <chimolilucy@gmail.com>
2021-07-04 16:49:48 +0300

    added files

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from django.contrib import admin
 
@@ -9,6 +10,9 @@ urlpatterns = [
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^detail/(\d+)', views.detail, name='detail'),
     url(r'^location/(\d+)', views.location, name='location'),
-    # url(r'^share/(\d+)', views.share, name='share'),
+    
   
 ]
+## new static route that references the location to the uploaded files
+if settings.DEBUG: 
+   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pictures/views.py
+++ pictures/views.py
@@ -5,7 +5,7 @@ from .models import Image,Location,Category
 # Create your views here.
 def image(request):
     image = Image.get_all()
-    return render(request,"all_images/foto.html", {"image":image})  
+    return render(request,"all_images/photo.html", {"image":image})  
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
