81d6f71e54ba4ba659eeb5b645d490f0cc165ff8
Moringa School <chimolilucy@gmail.com>
2021-07-05 10:11:49 +0300

    all files

--- pictures/tests.py
+++ pictures/tests.py
@@ -7,16 +7,19 @@ from .models import Location,Category,Image
 class LocationTestClass(TestCase):
     '''
     test for Location class
-    '''
+     '''
     # Set up method
     def setUp(self):
         self.loca= Location(country = 'Rwanda')
+
     def test_instance(self):
         self.assertTrue(isinstance(self.loca,Location))
+
     def test_save_method(self):
         self.loca.save_location()
         location = Location.objects.all()
         self.assertTrue(len(location) > 0)
+        
     def test_update(self):
         self.loca.save_location()
         location= Location.objects.filter(country="Rwanda").first()
@@ -33,12 +36,15 @@ class CategoryTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.cat = Category(category='Animmals')
+
     def test_instance(self):
         self.assertTrue(isinstance(self.cat,Category))
+
     def test_save_method(self):
         self.cat.save_category()
         category = Category.objects.all()
         self.assertTrue(len(category) > 0)
+
     def test_update(self):
         self.cat.save_category()
         category= Category.objects.filter(category='Animmals').first()
@@ -57,12 +63,15 @@ class ImageTestClass(TestCase):
         self.cat = Category(category = 'Animals')
         self.cat.save()
         self.new_image= Image(image = 'dog.jpg',name = 'twin dog',description = 'amazing dogs you can buy',location = self.loca,category = self.cat)
+
     def test_instance(self):
         self.assertTrue(isinstance(self.new_image,Image))
+
     def test_save_image(self):
         self.new_image.save()
         new_image = Image.objects.all()
         self.assertTrue(len(new_image) > 0)
+
     # testing update method
         
     def test_search_image(self):
@@ -71,11 +80,13 @@ class ImageTestClass(TestCase):
     def test_get_all_images(self):
         images = Image.objects.all()
         self.assertTrue(Image.name)
+
     # testing delete method
     def tearDown(self):
         Location.objects.all().delete()
         Category.objects.all().delete()
         Image.objects.all().delete()
+
     # testing get images by id Method
     def test_get_image_by_id(self):
         self.new_image.save_image()
